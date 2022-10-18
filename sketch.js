59ea10302e10f62127b90cc53ca42ebdb5f0dbe7
Cobie French <chf19@fsu.edu>
2022-10-18 10:43:46 -0400

    Messing around for class 10/19

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+  background(100,255,150);
 }
 
 function draw() {
-  background(125);
+  if (keyIsPressed === true) {
+    fill(0);
+  } else {
+    fill (255);
+  }
+  rect (25, 25, 50, 50);
+  noStroke()
+  fill(255, 204, 0)
+  triangle(100, 60, 160, 280, 320, 10)
+  fill (0, 0, 255)
+  rect(25, 280, 400, 50)
+  fill (255,0,0)
+  circle(300,200,100)
 }
